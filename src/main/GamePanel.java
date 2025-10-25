a63268b308af3fe7177d206d0794c56a85037c5b
Minh Nguyen <mtnguyen.dev@gmail.com>
2025-10-25 15:11:34 -0700

    initial commit

---
+++ src/main/GamePanel.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+
+	public GamePanel() {
+
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		g.drawOval(100, 100, 50, 50);
+	}
+}
